7403a4bc3ddebec538da0cbf01c03588cecf7a7e
Jackie <jfrager6532@student.pps.net>
2023-10-04 11:50:32 -0700

    Base Map With Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : Mover
+{
+    protected override void Start()
+    {
+        base.Start();
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        UpdateMotor(new Vector3(x, y, 0));
+    }
+
+    public void SwapSprite(int skinId)
+    {
+        GetComponent<SpriteRenderer>().sprite = GameManager.instance.playerSprite[skinId];
+    }
+}
