4b7dc95ae66c67aa04d0fae2d4ea44a89149f26f
Jackie <jfrager6532@student.pps.net>
2024-02-09 11:32:59 -0800

    dialoge buttons

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -36,15 +36,16 @@ public class DialogueManager : MonoBehaviour
         actorName.text = actorToDisplay.name;
         actorImage.sprite = actorToDisplay.sprite;
     }
-    public void NextMessage()
+    public void NextMessage(int mes)
     {
-        activeMessage++;
+        activeMessage = mes;
         if (activeMessage < currentMessages.Length) {
             DisplayMessage();
         }
         else {
             Debug.Log("Conversation ended");
             backgroundBox.LeanScale(Vector3.zero, .3f).setEaseInOutExpo();
+            activeMessage = 0;
             isActive = false;
         }
     }
@@ -58,8 +59,34 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isActive == true) {
-            NextMessage();
+        if (activeMessage == 0 && isActive == true) {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                NextMessage(1);
+                Debug.Log("1 pressed");
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                NextMessage(2);
+                Debug.Log("2 pressed");
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                NextMessage(3);
+                Debug.Log("3 pressed");
+            }
+
+        }
+        if (activeMessage != 0 && isActive == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                backgroundBox.LeanScale(Vector3.zero, .3f).setEaseInOutExpo();
+                activeMessage = 0;
+                isActive = false;
+                Debug.Log("Space pressed");
+            }
         }
     }
 }
